93f7165e9ed4b90a76cd482104f57d25ffcb3742
u-f-0 <carl@u-f-o.se>
2022-08-18 10:35:48 +0200

    Adds getSKUMap

--- products.go
+++ products.go
@@ -40,3 +40,37 @@ func GetProductsMap(maBaseURL string, token string) (map[string]Product, error) 
 	return maProductsMap, nil
 
 }
+
+func GetSkusMap(maBaseURL string, token string) (map[string]SKU, error) {
+
+	// Get all products from MA and make map
+	maSkusMap := make(map[string]SKU)
+	page := 1
+	for {
+		// convert page to str
+		pageString := strconv.Itoa(page)
+
+		// Get all SKUS
+		statusCode, response := Request("GET", maBaseURL+"/v1/skus?pageSize=1000&page="+pageString, token, nil)
+		if statusCode == http.StatusOK {
+			var maSkus []SKU
+			json.Unmarshal(response, &maSkus)
+
+			if len(maSkus) > 0 {
+				for _, sku := range maSkus {
+					maSkusMap[sku.Key] = sku
+				}
+				page += 1
+
+			} else {
+				break
+			}
+		} else {
+			err := errors.New("could not get skus from MA")
+			return nil, err
+		}
+	}
+
+	return maSkusMap, nil
+
+}
